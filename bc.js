5c1b1d1f6341ce998b54f2db79d1d96dd7628c58
bc200405925 Hazik Ahmed <bc200405925@vhyd03.local>
2024-06-11 13:15:25 +0500

    random colors are generated when we hover on the square and they stay

--- bc.js
+++ bc.js
@@ -18,12 +18,22 @@ function addElement() {
         const row = document.createElement("div");
         row.className = "row";
     
-    for(let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++){
         const square = document.createElement("div")
         square.className="square";
+        const setBg = () => {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            square.style.backgroundColor = "#" + randomColor;
+            color.innerHTML = "#" + randomColor;
+          }
+        square.addEventListener("mouseover",function(){
+            square.style.backgroundColor = setBg();
+        });
+        
         row.appendChild(square)
     }    
     main.appendChild(row)
     }
  }
+
 
