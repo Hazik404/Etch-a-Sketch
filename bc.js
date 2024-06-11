54907011e4ddcc0f586920da1588dbbd91f7322c
bc200405925 Hazik Ahmed <bc200405925@vhyd03.local>
2024-06-11 12:23:09 +0500

    the 16x16 grid is created and styled

--- bc.js
+++ bc.js
@@ -1,7 +1,11 @@
-document.body.onload = addElement;
+document.body.onload = () => {
+    addElement();
+    grid();
+}
 
 function addElement() {
     const main = document.createElement("div");
+    main.id = "main";
 
     const test = document.createTextNode("This is my main div");
 
@@ -9,3 +13,17 @@ function addElement() {
 
     document.body.appendChild(main);
 }
+ function grid() {
+    for(let i = 0; i < 16; i++){
+        const row = document.createElement("div");
+        row.className = "row";
+    
+    for(let i = 0; i < 16; i++){
+        const square = document.createElement("div")
+        square.className="square";
+        row.appendChild(square)
+    }    
+    main.appendChild(row)
+    }
+ }
+
